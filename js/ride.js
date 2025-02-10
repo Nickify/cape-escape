927fe8cc60ece91f355b2d11008956ddeb32eaae
Nick <73206781+Nickify@users.noreply.github.com>
2025-02-10 11:28:49 -0600

    Update ride.js

--- js/ride.js
+++ js/ride.js
@@ -72,7 +72,7 @@ WildRydes.map = WildRydes.map || {};
 
     function handlePickupChanged() {
         var requestButton = $('#request');
-        requestButton.text('Request Unicorn');
+        requestButton.text('Request Hero');
         requestButton.prop('disabled', false);
     }
 
